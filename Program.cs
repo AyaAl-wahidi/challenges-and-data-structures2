ee68dceaeee400c68ac78c3f3725711d7b082c86
Aya <ayanalwahidi@gmail.com>
2024-06-18 18:13:35 +0300

    Ch1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ArrayReversal();
         }
+
+        static void ArrayReversal()
+        {
+            Console.WriteLine("Please enter the length of the array you want to reverse:");
+            int arrLength = Convert.ToInt16(Console.ReadLine());
+
+            int [] arrNumber = new int[arrLength];
+            Console.WriteLine("Please enter the array values:");
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                arrNumber[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            Console.WriteLine("This is the reversed array:");
+            int[] reverseArray = new int[arrNumber.Length];
+            int j = 0;
+
+            foreach (int i in arrNumber)
+            {
+                reverseArray[j] = arrNumber[arrNumber.Length - 1 - j];
+                j++;
+            }
+            foreach (int i in reverseArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
     }
 }
